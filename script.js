fdc65b59b5b6f55fda922253ff598485b9af3da3
margalhao <margalhao21@gmail.com>
2026-07-10 18:45:31 -0300

    script commited

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function convertCurrency() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")  //valor em real
     const currencyValueConverted = document.querySelector(".currency-value") //valor convertido
-    
+
     const dollarToday = 5.12
 
     const convertedValue = inputCurrencyValue / dollarToday
@@ -12,12 +12,12 @@ function convertCurrency() {
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
-        }).format(inputCurrencyValue)
+    }).format(inputCurrencyValue)
 
     currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD"
-        }).format(convertedValue)
+    }).format(convertedValue)
 
 }
 
